811f99af3ea0edc7db3cacd6f69a0b9ced49731b
mwlsdotcom <github@mwls.com>
2019-11-30 15:18:58 +0000

    Add files via upload

---
+++ communication/how-to-ensure-you-receive-important-emails/index.php
@@ -0,0 +1,43 @@
+<?php #
+
+$title = "How to Ensure You Receive Important Emails";
+$titlecolor = "blue";
+$description = "Describes how you can ensure that you receive important emails";
+$keywords = "whitelist,mail,spam checker";
+$slogan = "Don't miss that important email";
+$slogancolor = "blue";
+$pagelogo = "scrnlogw_s.gif";
+$pagelength = 1.5;
+$googlesearch = "Whitelisting";
+
+include ('../../../header.php');
+
+?>
+
+<!-- CONTENT -->
+
+<p>Increasingly, filtering systems are trying and keep spam out of your inbox. Sometimes, they accidentally filter out the e-mails you want to receive. To make sure that important emails are not filtered into your 'junk' folder, you need to either provide your trusted sources with an unfiltered email address or have their addresses 'whitelisted'.</p>
+
+<p>Unfortunately, it is not always straightforward to do this and the process often depends on which email provider is used.</p>
+
+<p>For some, you place the trusted domains (e.g. mwls.com) on your <i>Safe List</i>. The safe list can be accessed via an 'Options' or 'Settings' link.</p>
+
+<p>For others, place the trusted domains (e.g. mwls.com) filter's 'whitelist'. You may need to search a filter's help for how to do this &#151; and depending on software/version, they may call a 'whitelist', a 'good list' or similar name.</p>
+
+<p>If an important email <i>is</i> filtered to your 'Spam' or 'Junk' folder, open the message and click on the 'this is not spam' link.</p>
+
+<p>If an important email is still being filtered, try adding the domain (e.g. mwls.com) or the email's 'From' or 'Reply-to' address to your address book or contact list. If this option is not available, try moving the message to your 'inbox', forwarding the message to yourself or replying to the email.</p>
+
+<p>If subsequent messages continue to be filtered, call or e-mail your ISP's tech support and specifically ask how you can be sure to receive all e-mail from your trusted domains (e.g. mwls.com). Follow their instructions for whitelisting.</p>
+
+<p>It's a shame to have to bother with all this, but unfortunately a few unscrupulous people make it difficult for those who want to conduct business responsibly on the Internet.</p>
+
+<p>Of course making sure that you can receive emails form trusted sources is only half the story. If you send emails there are some things you can do to reduce the chances of your emails being rejected by anti-spam software. For more information on this, please see our article on <a href=https://mwls.com/blog/business/how-to-ensure-your-customers-receive-important-emails/>
+How to Ensure Your Customers Receive Important Emails</a></p>
+
+<!-- end content -->
+
+<?php
+include ('../../../footer.php');
+?>
+
